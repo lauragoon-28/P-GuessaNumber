873d670a8e4e79ce79aa8c90c22ad06b5bf64a15
Laura Goon <Laura.goon@ou.edu>
2021-02-25 22:37:14 -0600

    finished participation

--- GuessANumber/Guess_a_Number/Program.cs
+++ GuessANumber/Guess_a_Number/Program.cs
@@ -13,55 +13,61 @@ namespace Guess_a_Number
             Console.WriteLine("Please enter a number that will be used as a minimum boundary. Whole numbers only.");
             string answer1 = Console.ReadLine();
 
-            if (int.TryParse(answer1, out minNum) == false)
+            while (int.TryParse(answer1, out minNum) == false)
             {
-                Console.WriteLine("Sorry, this is an invalid response");
+                Console.WriteLine("Sorry, this is an invalid response. Please enter a whole number");
+                answer1 = Console.ReadLine();
             }
 
             Console.WriteLine("Please enter a number that will be used as a maximum boundary. Whole numbers only.");
             string answer2 = Console.ReadLine();
 
-            if (int.TryParse(answer2, out maxNum) == false)
+            while (int.TryParse(answer2, out maxNum) == false)
             {
-                Console.WriteLine("Sorry, this is an invalid response");
+                Console.WriteLine("Sorry, this is an invalid response. Please enter a whole number");
+                answer2 = Console.ReadLine();
             }
 
             // so the guesser doesn't pick a max number that is greater than the min
             while (minNum >= maxNum) 
             {
-                Console.WriteLine("Your minimum number exceeds your maximum number. Please try again.");
+                Console.WriteLine($"Your minimum number exceeds your maximum number. Please input a new maximum value that is greater than {minNum}.");
+                answer2 = Console.ReadLine();
+
+                while (int.TryParse(answer2, out maxNum) == false)
+                {
+                    Console.WriteLine("Sorry, this is an invalid response. Please enter a whole number");
+                    answer2 = Console.ReadLine();
+                }
             }
 
             // generate random number
             Random rand = new Random();
-            randNum = rand.Next(minNum, maxNum);
+            randNum = rand.Next(minNum, maxNum + 1);
 
-            Console.WriteLine("Please guess a number between your minimum and maximum, guess until you get it right!");
+            Console.WriteLine("Please guess a number between your minimum and maximum values, guess until you get it right!");
             string answer3 = Console.ReadLine();
             int guess;
 
             // loop for guessing random number
-            
-            bool isAnsweranInt = int.TryParse(answer3, out guess);
-            while (true )
+
+            while (int.TryParse(answer3, out guess) == false)
             {
-                if (guess == randNum)
+                Console.WriteLine("Invalid guess. Please enter a whole number");
+                answer3 = Console.ReadLine();
+            }
+            while (guess != randNum)
+            {
+                Console.WriteLine("Your guess is incorrect. Keep on trying!!");
+                answer3 = Console.ReadLine();
+                while (int.TryParse(answer3, out guess) == false)
                 {
-                    Console.WriteLine("Congrats! You guessed right! Yay!");
-                    return;
-                }
-                else
-                {
-                    Console.WriteLine("Keep on guessing! You'll get it eventually!");
+                    Console.WriteLine("Invalid guess. Please enter a whole number");
                     answer3 = Console.ReadLine();
                 }
             }
-            while (isAnsweranInt == false)
-            {
-                guess = Convert.ToInt32(answer3);
-                Console.WriteLine("You guessed wrong.");
-                Console.WriteLine("Enter another number");
-            }
+
+            Console.WriteLine("Congrats! You guessed it right!!! YAY!! Go you!!! :))))");
             
         }
     }
